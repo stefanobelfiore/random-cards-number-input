6fe27ffbc9b5592bb89cedbf1dd9ea0eb205df5e
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-05-18 17:31:42 +0000

    falta el bubble

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import "./style.css";
 
 const INPUT = document.querySelector("#inputOfNumberOfCards");
 const BUTTON = document.querySelector("#generatorCard");
+const selectButton = document.querySelector("#selection");
+const bubbleButton = document.querySelector("#bubble");
+let myArray = [];
 const VALUES = [
   "A",
   "2",
@@ -21,24 +24,31 @@ const VALUES = [
 ];
 const SUITS = ["♠", "♣", "♥", "♦"];
 const SECTION_TO_DRAW = document.querySelector("#drawnCardsContainer");
+const SECTION_TO_DRAW_SELECTED = document.querySelector("#drawnCardsSelected");
 
 window.onload = function() {
   getCards();
+  selectSort();
 };
 
 const getCards = () => {
   BUTTON.addEventListener("click", event => {
     event.preventDefault();
     SECTION_TO_DRAW.innerHTML = "";
+
     for (let index = 0; index < INPUT.value; index++) {
-      drawCard(generateCards());
+      let myCard = generateCards();
+      drawCard(myCard, SECTION_TO_DRAW);
+      myArray.push(myCard);
     }
   });
 };
 
 const generateCards = () => {
+  let id = getRandom(VALUES.length);
   let card = {
-    value: VALUES[getRandom(VALUES.length)],
+    id: id,
+    value: VALUES[id],
     suit: SUITS[getRandom(SUITS.length)]
   };
   return card;
@@ -48,7 +58,7 @@ const getRandom = maxNumber => {
   return Math.floor(Math.random() * maxNumber);
 };
 
-const drawCard = card => {
+const drawCard = (card, section) => {
   let cardBody = document.createElement("div");
   cardBody.classList.add("card");
 
@@ -79,5 +89,45 @@ const drawCard = card => {
   cardBody.appendChild(valueContainer);
   cardBody.appendChild(secondSuitContainer);
 
-  SECTION_TO_DRAW.appendChild(cardBody);
+  section.appendChild(cardBody);
 };
+
+const selectSort = () => {
+  selectButton.addEventListener("click", () => {
+    console.log("gggggggggggggggggggggggg");
+    let min = 0;
+    let arr = myArray;
+    while (min < arr.length) {
+      for (let i = min + 1; i < arr.length; i++) {
+        if (arr[min].id > arr[i].id) {
+          let aux = arr[min];
+          arr[min] = arr[i];
+          arr[i] = aux;
+        }
+      }
+      min++;
+    }
+    myArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      drawCard(arr[i], SECTION_TO_DRAW_SELECTED);
+    }
+  });
+};
+
+// const bubbleSort = (arr) => {
+//     let wall = arr.length - 1; //we start the wall at the end of the array
+//     while (wall > 0){
+//         let index = 0;
+//         while (index < wall) {
+//           //compare the adjacent positions, if the right one is bigger, we have to swap
+//           if (arr[index] > arr[index + 1]) {
+//             let aux = arr[index];
+//             arr[index] = arr[index + 1];
+//             arr[index + 1] = aux;
+//           }
+//           index++;
+//         }
+//         wall--; //decrease the wall for optimization
+//     }
+// 	return arr;
+// };
